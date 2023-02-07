7152881f322225f7398cc8a21ab8f610268db8f8
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-02-07 10:37:31 -0800

    d

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const AES = require("crypto-js/aes");
 const http = require('http');
 const fs = require('fs');
 
-http
+const server = http.createServer(async (req,res) => {
+    var url = new URL(`http://websi.te${req.url}`);
+    if(url.pathname == '/api/verify') {
+        u
+    }
+});
